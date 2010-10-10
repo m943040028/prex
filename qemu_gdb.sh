a4e4f7a5cf7c85e547e0a9ea0199efa11de09fdd
shengyu <shengyu@shengyu-laptop.(none)>
2010-10-10 15:22:02 +0800

    code sync

--- qemu_gdb.sh
+++ qemu_gdb.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 qemu-system-ppcemb -nographic -M bamboo -kernel prexos -s -S&
-powerpc-amcc-elf-insight sys/prex ; pkill qemu-system-ppcemb
+powerpc-unknown-elf-gdb sys/prex ; pkill qemu-system-ppcemb
